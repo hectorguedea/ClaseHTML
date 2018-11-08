32798613bfcc9603f814f81f8ac4482ac4128077
Héctor Guedea <hectorguedea@gmail.com>
2018-11-08 09:19:59 -0600

    HTML a PHP, terminó personal page

--- personal.php
+++ personal.php
@@ -84,13 +84,17 @@
             text-align: justify;
         }
 
-        .container a {
+        .container a, footer a {
             color: #cfca77;
             text-decoration: none;
         }
 
-        .container a:hover {
+        .container a:hover, footer a:hover {
             color: #bbb7b7;
+        }
+
+        #descripcion {
+            overflow: hidden;
         }
 
         #descripcion img {
@@ -99,10 +103,70 @@
             float: left;
             margin-right: 10px;
         }
+
+        #tecnologias {
+            margin-top: 15px;
+        }
+
+        .lenguajes {
+            margin-top: 20px;
+            overflow: hidden;
+
+        }
+
+        .lenguajes img {
+            width: 125px;
+            height: auto;
+            float: left;
+            margin-right: 10px;
+        }
+        #contacto form {
+            padding:20px;
+            background-color:#d79423;
+        }
+        #contacto form input, #contacto form textarea{
+            display:block;
+            margin-bottom:10px;
+            padding:5px;
+            font-size:15px;
+        }
+        #contacto form textarea{
+            width:300px;
+            height:100px;
+        }
+        #contacto form input[type=submit]{
+            cursor:pointer;
+            padding:10px;
+            font-size:16px;
+            background-color:#cfca77;
+            box-shadow: 7px 7px 30px -9px rgba(0,0,0,0.75);
+            margin-bottom:0;
+        }
+         #contacto form input[type=submit]:hover{
+         background-color:#f4f1b9;
+         }
+
+         footer{
+          background-color: #f9f8e2;
+          text-align:center;
+          padding:15px;
+          margin-top:20px;
+         }
+         footer p{
+             margin-bottom:3px;
+             margin-top:3px;
+            /* margin:3px 0;*/
+         }
+       
     </style>
 </head>
 
 <body>
+    <!-- 
+        Página Online:
+
+        https://personal--hectorguedea.repl.co
+    -->
 
     <header>
         <div id="header">
@@ -115,6 +179,7 @@
             </nav>
         </div>
     </header>
+
 
     <div class="container">
         <h1>Software Engineer</h1>
@@ -129,16 +194,59 @@
                 extra attributes such as internal and external team communication, and able to adapt quickly to changes
                 in requirements
                 and innovations in programming languages.</p>
+
+            <?php
+            //Declaración de variables
+            $nombre ='Héctor Guedea';
+            $edad = 40;
+            $club = "Club de Hacking";
+            $donde = "Talentos";
+            
+            echo '<p>';
+            echo 'El maestro '.$nombre.' tiene '.$edad.' e imparte el '.$club.' en '.$donde.'.'; 
+            echo '</p>';
+            ?>
             <p><strong>Keywords:</strong> <a href="https://www.google.com/search?source=hp&ei=vQXSW7bRM4fMwQL5qqiABw&q=web+development+&oq=web+development+&gs_l=psy-ab.3...2600.5452.0.5703.12.9.0.0.0.0.310.802.2-2j1.3.0....0...1c.1.64.psy-ab..10.2.519.0..0j35i39k1j0i131k1.0.5x6BtbAAb24"
                     title="Buscar en Google Web Development">web development</a>, <a href="https://www.google.com/search?ei=xQXSW4PYE4nUwQLnwqywBA&q=frontend+development&oq=frontend+development&gs_l=psy-ab.3..35i39k1j0l2j0i10k1j0j0i203k1j0i10i203k1j0i203k1l2j0i10i203k1.273358.280485.0.280636.30.24.4.0.0.0.493.3076.0j1j2j4j2.9.0....0...1c.1.64.psy-ab..20.10.2143...0i67k1j0i131k1.0.90Y7hCoJmT8"
                     title="Frontend">frontend</a>, <a href="https://www.google.com/search?ei=3gbSW4PaM4fWkwXhwKTYCw&q=backend+development&oq=backend+development&gs_l=psy-ab.3..0i7i30k1l10.27801.28640.0.28830.7.7.0.0.0.0.236.236.2-1.1.0....0...1c.1.64.psy-ab..6.1.235....0.7BldP3cZL6Q"
                     title="Backend Development"> backend</a></p>
+        </section>
+        <section id="tecnologias">
+            <h1>Lenguajes de programación</h1>
+            <p>Tengo experiencia con los siguientes <i>lenguajes de programación</i>, desarrollado diversos proyectos.
+            </p>
 
+            <div class="lenguajes">
+                <img src="https://image.flaticon.com/icons/png/512/490/490273.png" title="Algoritmos" />
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/230px-HTML5_logo_and_wordmark.svg.png"
+                    title="HTML" />
+                <img src="https://lenguajecss.com/img/css3-logo.png" title="CSS" />
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/1200px-PHP-logo.svg.png"
+                    title="PHP" />
+            </div>
+
+        </section>
+        <section id="contacto">
+            <h1>¡Ponte en contacto conmigo!</h1>
+            <p>Estoy abierto a oportunidades laborales como <strong>software engineer</strong>.</p>
+            <form method="POST" action="personal.html">
+                <input type="text" placeholder="Agrega tu nombre" name="nombre" required>
+                <input type="text" placeholder="Agrega el asunto" name="asunto" required>
+                <input type="email" placeholder="Agrega tu email" name="email" required>
+                <textarea name="mensaje" placeholder="Agrega tu mensaje" required></textarea>
+                <input type="submit" value="Enviar mensaje">
+            </form>
 
         </section>
 
 
+
     </div>
+    <footer>
+        <p> <strong>Code is the new poem.</strong></p>
+        <p> <a href="mailto:hectorguedea@gmail.com">Contact me, now!</a> </p>
+        Héctor Guedea
+    </footer>
 
 </body>
 
